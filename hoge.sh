676befb814882a6f4b911a93d627e8cd2e5d6ade
t-yamamura <t_yamamura@pluto.ai.kyutech.ac.jp>
2021-05-20 00:17:31 +0900

    Delete hoge.sh

--- hoge.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-TEST_RESOURCES_DIR="tests/resources/"
-for DIC_TYPE in {system,user,user2}; do
-  IN="${TEST_RESOURCES_DIR}${DIC_TYPE}.csv"
-  OUT="${TEST_RESOURCES_DIR}${DIC_TYPE}.dic"
-  DES="the ${DIC_TYPE} dictionary for the unit tests"
-  python -c "import sys; from chikkarpy.command_line import build_dictionary; build_dictionary(sys.argv[0], sys.argv[1], sys.argv[2]);" "${IN}" "${OUT}" "${DES}"
-done
