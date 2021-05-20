39061d5c0d56f2148b99ea92d3038e0893522e3a
t-yamamura <t_yamamura@pluto.ai.kyutech.ac.jp>
2021-05-20 18:33:00 +0900

    update config

--- chikkarpy/config.py
+++ chikkarpy/config.py
@@ -33,8 +33,8 @@ def download_dictionary():
     _, _msg = urlretrieve(ZIP_URL, ZIP_NAME)
     with ZipFile(ZIP_NAME) as z:
         z.extractall()
-    logger.warning("UNZIP_NAME", os.path.abspath(UNZIP_NAME), UNZIP_NAME, flush=True)
-    logger.warning(DEFAULT_RESOURCEDIR, flush=True)
+    logger.warning("UNZIP_NAME", os.path.abspath(UNZIP_NAME), UNZIP_NAME)
+    logger.warning(DEFAULT_RESOURCEDIR)
     os.rename(UNZIP_NAME, DEFAULT_RESOURCEDIR)
 
     os.remove(ZIP_NAME)
