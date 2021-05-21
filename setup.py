b6b22c8a1258c06928dc9e5340aa47e755a7bb55
t-yamamura <t_yamamura@pluto.ai.kyutech.ac.jp>
2021-05-21 20:41:17 +0900

    fix format violation

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License
+# limitations under the License.
 
 from chikkarpy.config import download_dictionary
 
@@ -37,4 +37,3 @@ setup(
         "sortedcontainers~=2.1.0"
     ]
 )
-
