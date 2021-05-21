7777f28b4a26af682e5096179f0c987af01b5a7c
t-yamamura <t_yamamura@pluto.ai.kyutech.ac.jp>
2021-05-21 20:38:24 +0900

    fix format.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
-flake8 --show --config=flake8.cfg ../chikkarpy
-flake8 --show --config=flake8.cfg ../tests
+flake8 --show --config=flake8.cfg ../setup.py ../chikkarpy ../tests >> .log 2>&1
 
 HEADER=$(cat license-header.txt)
 
 cd ..
 
 array=()
-
 for FILE in $(find ./chikkarpy -type f -name "*.py"); do
     array+=( ${FILE} )
 done
-
 for FILE in $(find ./tests -type f -name "*.py"); do
     array+=( ${FILE} )
 done
-
 array+=( ./setup.py )
 
 for FILE in ${array[@]}; do
     FILE_CONTENTS=$(cat "${FILE}")
     if [[ ${FILE_CONTENTS} != ${HEADER}* ]]; then
-        >&2 echo "invalid license header on ${FILE}"
+        echo "invalid license header on ${FILE}" >> scripts/.log 2>&1
     fi
 done
+
+cat scripts/.log
+ERROR_LINE_NUM=$(cat scripts/.log | wc -l)
+rm scripts/.log
+
+if [ "${ERROR_LINE_NUM}" -gt 0 ]; then
+  exit 1
+fi
